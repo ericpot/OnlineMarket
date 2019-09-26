fffccaba672c359046cbc1e676d2d66ed2cc9863
Faiz <faiz.rashid@u.nus.edu>
2019-09-27 01:33:11 +0800

    version_1

--- OnlineMarket.sql
+++ OnlineMarket.sql
@@ -2,58 +2,60 @@ create database OnlineMarket;
 use OnlineMarket;
 
 create table Users (
-    userID INT AUTO_INCREMENT PRIMARY KEY, 
+    userID INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL, 
     address VARCHAR(255) NOT NULL, 
-    email VARCHAR(255) not null UNIQUE, 
-    username VARCHAR(60) not null UNIQUE, 
-    password VARCHAR(255) not null  UNIQUE, 
+    email VARCHAR(255) NOT NULL UNIQUE, 
+    username VARCHAR(60) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, 
     createdDate TIMESTAMP, 
 );
 
 CREATE TABLE Carts(
-orderID INT auto_increment primary key, 
-productID INT AUTO_INCREMENT not null unique, 
-qty INT
+orderID INT AUTO_INCREMENT PRIMARY KEY, 
+productID INT AUTO_INCREMENT NOT NULL UNIQUE, 
+qty INT NOT NULL, 
+CHECK (qty > 0), 
 FOREIGN KEY (orderID) REFERENCES Users (userID)
-FOREIGN KEY (productID) REFERENCES Users (userID)
+FOREIGN KEY (productID) REFERENCES Products (productID)
 );
 
 CREATE TABLE Wishlist(
 userID INT, 
 productID INT,
-FOREIGN KEY (productID) REFERENCES Products (productID)
+FOREIGN KEY (productID) REFERENCES Products (productID) ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (userID) REFERENCES Users (UserID),
 PRIMARY KEY (userID, productID)
 );
 
 
 CREATE TABLE CustomerReview(
-messageID INT auto_increment PRIMARY KEY, 
-sellerID INT, 
+messageID INT AUTO_INCREMENT PRIMARY KEY, 
+sellerID INT AUTO_INCREMENT, 
 message VARCHAR(255),
 FOREIGN KEY (sellerID) REFERENCES Users (userID)
 );
 
 CREATE TABLE Products(
-    productID INT auto_increment PRIMARY KEY, 
-    sellerID INT auto_increment, 
-    productName VARCHAR (255), 
-    description VARCHAR(256), 
-    category VARCHAR(60), 
+    productID INT AUTO_INCREMENT PRIMARY KEY, 
+    sellerID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    productName VARCHAR (255) NOT NULL, 
+    description VARCHAR(256) NOT NULL, 
+    category VARCHAR(60) NOT NULL, 
     qty INT auto_increment, 
     price DOUBLE, 
-    size_color VARCHAR(255), 
-    image VARCHAR (255), 
-    createdDate TIMESTAMP
+    image VARCHAR (255) NOT NULL, 
+    createdDate TIMESTAMP,
+    CHECK (qty > 0), 
+    CHECK (price > 0), 
     FOREIGN KEY (sellerID) REFERENCES Users (userID)
 );
 
 CREATE TABLE Orders(
-    orderID INT PRIMARY KEY, 
-    userID INT, 
-    paymentMode VARCHAR(255), 
-    status VARCHAR(20), 
+    orderID INT AUTO_INCREMENT PRIMARY KEY, 
+    userID INT UNIQUE NOT NULL, 
+    paymentMode VARCHAR(60) NOT NULL, 
+    status VARCHAR(20) NOT NULL, 
     createDate TIMESTAMP, 
     FOREIGN KEY (userID) REFERENCES Users (userID)
 );
@@ -61,8 +63,9 @@ CREATE TABLE Orders(
 CREATE OrderDetails(
     productID INT, 
     orderID INT, 
-    qty INT, 
-    FOREIGN KEY (userID) REFERENCES Orders(userID)
+    qty INT NOT NULL, 
+    CHECK (qty > 0), 
+    FOREIGN KEY (productID) REFERENCES Products(productID)
     FOREIGN KEY (orderID) REFERENCES Orders(orderID)
     PRIMARY KEY (productID, orderID)
 );

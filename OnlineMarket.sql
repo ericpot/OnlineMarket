1c1e74dcf4c04bd6cf8a496ec1d505fc1fe3b423
Faiz <faiz.rashid@u.nus.edu>
2019-09-28 11:15:28 +0800

    version_4

--- OnlineMarket.sql
+++ OnlineMarket.sql
@@ -1,8 +1,8 @@
+DROP DATABASE IF EXISTS `OnlineMarket`;
 CREATE DATABASE IF NOT EXISTS OnlineMarket;
 use OnlineMarket;
 
 DROP TABLE IF EXISTS `Users`;
-CREATE TABLE IF NOT EXISTS `Users`;
 
 CREATE TABLE Users (
     userID INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,41 +14,7 @@ CREATE TABLE Users (
     createdDate TIMESTAMP
 );
 
-DROP TABLE IF EXISTS `Carts`;
-CREATE TABLE IF NOT EXISTS `Carts`;
-
-CREATE TABLE Carts (
-orderID INT AUTO_INCREMENT PRIMARY KEY, 
-productID INT NOT NULL UNIQUE, 
-qty INT NOT NULL, 
-CHECK (qty > 0), 
-FOREIGN KEY (orderID) REFERENCES Users (userID),
-FOREIGN KEY (productID) REFERENCES Products (productID)
-);
-
-DROP TABLE IF EXISTS `Wishlist`;
-CREATE TABLE IF NOT EXISTS `Wishlist`;
-
-CREATE TABLE Wishlist (
-userID INT, 
-productID INT,
-FOREIGN KEY (productID) REFERENCES Products (productID) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (userID) REFERENCES Users (UserID),
-PRIMARY KEY (userID, productID)
-);
-
-DROP TABLE IF EXISTS `CustomerReview`;
-CREATE TABLE IF NOT EXISTS `CustomerReview`;
-
-CREATE TABLE CustomerReview (
-messageID INT AUTO_INCREMENT PRIMARY KEY, 
-sellerID INT, 
-message VARCHAR(255),
-FOREIGN KEY (sellerID) REFERENCES Users (userID)
-);
-
 DROP TABLE IF EXISTS `Products`;
-CREATE TABLE IF NOT EXISTS `Products`;
 
 CREATE TABLE Products (
     productID INT AUTO_INCREMENT PRIMARY KEY, 
@@ -65,8 +31,37 @@ CREATE TABLE Products (
     FOREIGN KEY (sellerID) REFERENCES Users (userID)
 );
 
+DROP TABLE IF EXISTS `Carts`;
+
+CREATE TABLE Carts (
+orderID INT AUTO_INCREMENT PRIMARY KEY, 
+productID INT NOT NULL UNIQUE, 
+qty INT NOT NULL, 
+CHECK (qty > 0), 
+FOREIGN KEY (orderID) REFERENCES Users (userID),
+FOREIGN KEY (productID) REFERENCES Products (productID)
+);
+
+DROP TABLE IF EXISTS `Wishlist`;
+
+CREATE TABLE Wishlist (
+userID INT, 
+productID INT,
+FOREIGN KEY (productID) REFERENCES Products (productID) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (userID) REFERENCES Users (UserID),
+PRIMARY KEY (userID, productID)
+);
+
+DROP TABLE IF EXISTS `CustomerReview`;
+
+CREATE TABLE CustomerReview (
+messageID INT AUTO_INCREMENT PRIMARY KEY, 
+sellerID INT, 
+message VARCHAR(255),
+FOREIGN KEY (sellerID) REFERENCES Users (userID)
+);
+
 DROP TABLE IF EXISTS `Orders`;
-CREATE TABLE IF NOT EXISTS `Orders`;
 
 CREATE TABLE Orders (
     orderID INT AUTO_INCREMENT PRIMARY KEY, 
@@ -78,7 +73,6 @@ CREATE TABLE Orders (
 );
 
 DROP TABLE IF EXISTS `OrderDetails`;
-CREATE TABLE IF NOT EXISTS `OrderDetails`;
 
 CREATE TABLE OrderDetails (
     productID INT, 
